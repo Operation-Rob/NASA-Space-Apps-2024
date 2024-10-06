14ab6112036fd2fa07668da8f2afd34eb342cde4
Michael Nef <michael@nef.net.au>
2024-10-06 23:51:19 +0800

    fixed build step error

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -5,6 +5,7 @@ export interface Pin {
   lng: number;
   name: string;
   loading: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any; // Replace 'any' with the actual data type
   error: string | null; // Replace 'any' with the actual error type
 }
