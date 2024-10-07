9dd3a2fca2ec9d5f47d7878a302b7858c8b29102
cjnbennett <conor.jn.bennett@gmail.com>
2024-10-07 11:34:00 +0800

    updates

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -6,6 +6,6 @@ export interface Pin {
   name: string;
   loading: boolean;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: any; // Replace 'any' with the actual data type
+  data: number[]; // Replace 'any' with the actual data type
   error: string | null; // Replace 'any' with the actual error type
 }
